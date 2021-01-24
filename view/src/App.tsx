e3f2fbcf1f516f4ff8a5dd6edb8939cb4c3016aa
ferueda <fe.rueda@outlook.com>
2021-01-24 00:29:57 -0300

    Update main application

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { ROUTES } from './utils/constants';
+import { Container } from '@chakra-ui/react';
+
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+
+import Nav from './components/Nav';
 
 function App() {
-  return <div>My App</div>;
+  return (
+    <Container maxWidth={1200} minHeight="100vh" mx="auto">
+      <Nav />
+
+      <Container as="main" display="flex" minHeight="100%">
+        <Switch>
+          <Route exact path={ROUTES.home}>
+            <Home />
+          </Route>
+
+          <Route path={ROUTES.signup}>
+            <SignUp />
+          </Route>
+
+          <Route path="*">
+            <Redirect to={ROUTES.home} />
+          </Route>
+        </Switch>
+      </Container>
+    </Container>
+  );
 }
 
 export default App;
--- view/src/index.tsx
+++ view/src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider, GlobalStyle } from '@chakra-ui/react';
+import theme from './styles/theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
