dafba3517dfddddbadb23f76ce003c0191e8fcd2
ferueda <fe.rueda@outlook.com>
2021-01-25 13:52:48 -0300

    Add ENDPOINTS constant

--- view/src/utils/constants.ts
+++ view/src/utils/constants.ts
@@ -2,3 +2,8 @@ export const ROUTES = {
   home: '/',
   signup: '/signup',
 };
+
+export const ENDPOINTS = {
+  signUp: 'https://us-central1-microservices-example-302620.cloudfunctions.net/signUp',
+  dev: 'http://localhost:8080/',
+};
