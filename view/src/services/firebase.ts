0b281c4a06aa871c8a79db9066e22a08ec6e6066
ferueda <fe.rueda@outlook.com>
2021-01-25 13:52:32 -0300

    Add getCurrentUser and deleteUser functions

--- view/src/services/firebase.ts
+++ view/src/services/firebase.ts
@@ -4,8 +4,19 @@ import { firebaseConfig } from '../utils/config';
 
 firebase.initializeApp(firebaseConfig);
 
-export async function signUpNewUserWithEmailAndPassword(email: string, password: string) {
+export async function signUpNewUserWithEmailAndPassword(
+  email: string,
+  password: string,
+): Promise<firebase.auth.UserCredential> {
   return await firebase.auth().createUserWithEmailAndPassword(email, password);
+}
+
+export function getCurrentUser(): firebase.User | null {
+  return firebase.auth().currentUser;
+}
+
+export async function deleteUser(user: firebase.User) {
+  return await user.delete();
 }
 
 export default firebase;
