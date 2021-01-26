acfd4bc009dcf6a2ee84f3fea9de601571233f8f
ferueda <fe.rueda@outlook.com>
2021-01-26 12:41:13 -0300

    Add authContext and auth state to the App

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -6,28 +6,37 @@ import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 
 import Nav from './components/Nav';
+import Login from './pages/Login';
+
+import AuthContextProvider from './globalState/authContext';
 
 function App() {
   return (
-    <Container maxWidth={1200} minHeight="100vh" mx="auto">
-      <Nav />
+    <AuthContextProvider>
+      <Container maxWidth={1200} minHeight="100vh" mx="auto">
+        <Nav />
 
-      <Container as="main" display="flex" minHeight="100%">
-        <Switch>
-          <Route exact path={ROUTES.home}>
-            <Home />
-          </Route>
+        <Container as="main" display="flex" minHeight="100%">
+          <Switch>
+            <Route exact path={ROUTES.home}>
+              <Home />
+            </Route>
 
-          <Route path={ROUTES.signup}>
-            <SignUp />
-          </Route>
+            <Route path={ROUTES.signup}>
+              <SignUp />
+            </Route>
 
-          <Route path="*">
-            <Redirect to={ROUTES.home} />
-          </Route>
-        </Switch>
+            <Route path={ROUTES.login}>
+              <Login />
+            </Route>
+
+            <Route path="*">
+              <Redirect to={ROUTES.home} />
+            </Route>
+          </Switch>
+        </Container>
       </Container>
-    </Container>
+    </AuthContextProvider>
   );
 }
 
