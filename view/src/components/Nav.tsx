42f21278be7b4c9538f562a268906da6041d2b46
ferueda <fe.rueda@outlook.com>
2021-01-26 12:41:48 -0300

    Add authContext and auth state to Nav
    Added the auth state and logic that handles what to show based on if there is a user or not

--- view/src/components/Nav.tsx
+++ view/src/components/Nav.tsx
@@ -1,21 +1,47 @@
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
 import { ROUTES } from '../utils/constants';
+import { Container, Text } from '@chakra-ui/react';
 
-import { Container } from '@chakra-ui/react';
+import NavLink from './shared/NavLink';
+
+import { AuthContext } from '../globalState/authContext';
+import { logOutCurrentUser } from '../services/firebase';
 
 function Nav() {
+  const [user, setUser] = useContext<any>(AuthContext);
+
+  const handleLogout = () => {
+    logOutCurrentUser()
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <Container
       as="nav"
       mx="auto"
+      py={4}
       maxWidth={800}
       centerContent
       display="flex"
       flexDir="row"
       justifyContent="space-evenly"
     >
-      <Link to={ROUTES.home}>Home</Link>
-      <Link to={ROUTES.signup}>Sign Up</Link>
+      {user ? (
+        <>
+          <NavLink to={ROUTES.home}>Home</NavLink>
+          <Text as="button" onClick={handleLogout} _hover={{ color: 'blue.500' }}>
+            Log out
+          </Text>
+        </>
+      ) : (
+        <>
+          <NavLink to={ROUTES.signup}>Sign Up</NavLink>
+          <NavLink to={ROUTES.login}>Log In</NavLink>
+        </>
+      )}
     </Container>
   );
 }
