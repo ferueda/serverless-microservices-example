923140e1573a4dc1b13c70eb3c45153a36525063
ferueda <fe.rueda@outlook.com>
2021-01-26 12:41:58 -0300

    Add authContext and auth state to Home

--- view/src/pages/Home.tsx
+++ view/src/pages/Home.tsx
@@ -1,4 +1,16 @@
+import { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+
+import { AuthContext } from '../globalState/authContext';
+import { ROUTES } from '../utils/constants';
+
 function Home() {
+  const [user] = useContext(AuthContext);
+
+  if (!user) {
+    return <Redirect to={ROUTES.login} />;
+  }
+
   return <div>This is the Home page</div>;
 }
 
