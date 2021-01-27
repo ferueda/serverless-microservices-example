5323d05694d9a45c08fd01ffd364d282678f3a0f
ferueda <fe.rueda@outlook.com>
2021-01-27 19:10:04 -0300

    Improve UI and add log in link

--- view/src/components/Nav.tsx
+++ view/src/components/Nav.tsx
@@ -1,48 +1,61 @@
 import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import { ROUTES } from '../utils/constants';
-import { Container, Text } from '@chakra-ui/react';
+import { Box, Text, Icon } from '@chakra-ui/react';
 
 import NavLink from './shared/NavLink';
 
 import { AuthContext } from '../globalState/authContext';
 import { logOutCurrentUser } from '../services/firebase';
 
+import { BsFillPersonFill } from 'react-icons/bs';
+
 function Nav() {
   const [user, setUser] = useContext<any>(AuthContext);
+
+  const history = useHistory();
 
   const handleLogout = () => {
     logOutCurrentUser()
       .then(() => {
         setUser(null);
+        history.push(ROUTES.login);
       })
       .catch((error) => console.log(error));
   };
 
   return (
-    <Container
+    <Box
       as="nav"
-      mx="auto"
       py={4}
-      maxWidth={800}
-      centerContent
+      px={12}
+      width="100%"
       display="flex"
-      flexDir="row"
-      justifyContent="space-evenly"
+      justifyContent="space-between"
+      shadow="md"
     >
       {user ? (
         <>
-          <NavLink to={ROUTES.home}>Home</NavLink>
-          <Text as="button" onClick={handleLogout} _hover={{ color: 'blue.500' }}>
-            Log out
-          </Text>
+          <Box>
+            <NavLink to={ROUTES.home}>Home</NavLink>
+          </Box>
+          <Box d="flex" alignItems="center">
+            <Box d="flex" alignItems="center" mx={2}>
+              <Icon as={BsFillPersonFill} w={5} h={5} mr={1} color="gray.600" />
+              <Text>{user?.first_name}</Text>
+            </Box>
+            <Text as="button" onClick={handleLogout} _hover={{ color: 'blue.500' }} mx={2}>
+              Log out
+            </Text>
+          </Box>
         </>
       ) : (
-        <>
-          <NavLink to={ROUTES.signup}>Sign Up</NavLink>
+        <Box ml="auto">
           <NavLink to={ROUTES.login}>Log In</NavLink>
-        </>
+          <NavLink to={ROUTES.signup}>Sign Up</NavLink>
+        </Box>
       )}
-    </Container>
+    </Box>
   );
 }
 
