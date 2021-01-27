02920a275a044a2d7acd65966a32ef4a6a293706
ferueda <fe.rueda@outlook.com>
2021-01-27 19:11:27 -0300

    Refactor SignUp
    Added loading state
    Added redirect when user exists

--- view/src/pages/SignUp.tsx
+++ view/src/pages/SignUp.tsx
@@ -1,40 +1,60 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { Redirect, useHistory } from 'react-router-dom';
 import { Container, Heading, Text } from '@chakra-ui/react';
+import { ROUTES } from '../utils/constants';
+
 import { signUpNewUserWithEmailAndPassword } from '../services/firebase';
-import { postNewUserToDb } from '../services/api';
-import { UserData } from '../interfaces/IuserData';
 
 import FormInput from '../components/Form/FormInput';
 import FormButton from '../components/Form/FormButton';
 import Form from '../components/Form/Form';
+
 import Link from '../components/shared/Link';
 
-import { ROUTES } from '../utils/constants';
+import { AuthContext } from '../globalState/authContext';
+import { IUserData } from '../types/IUser';
 
 function SignUp() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [user, setUser] = useContext(AuthContext);
+  const history = useHistory();
 
   const handleSubmit = (e: React.FormEvent<HTMLDivElement>): void => {
     e.preventDefault();
 
-    signUpNewUserWithEmailAndPassword(email, password)
-      .then(({ user }) => {
-        const userData: UserData = {
-          email: user?.email,
-          uid: user?.uid,
-        };
+    setIsLoading(true);
 
-        return postNewUserToDb(userData);
-      })
-      .then(() => {
+    const userData: IUserData = {
+      email,
+      password,
+      first_name: firstName,
+      last_name: lastName,
+    };
+
+    signUpNewUserWithEmailAndPassword(userData)
+      .then((signedUpUser) => {
         setEmail('');
         setPassword('');
+        setFirstName('');
+        setLastName('');
+        setIsLoading(false);
+        setUser(signedUpUser);
+        history.push(ROUTES.home);
       })
       .catch((error) => {
         console.log(error);
+        setIsLoading(false);
       });
   };
+
+  if (user) {
+    return <Redirect to={ROUTES.home} />;
+  }
 
   return (
     <Container
@@ -51,11 +71,31 @@ function SignUp() {
         </Heading>
 
         <FormInput
+          label="First name"
+          id="firstName"
+          placeholder="John"
+          value={firstName}
+          onChange={({ target }) => setFirstName(target.value)}
+          required
+        />
+
+        <FormInput
+          label="Last name"
+          id="lastName"
+          placeholder="Doe"
+          value={lastName}
+          onChange={({ target }) => setLastName(target.value)}
+          required
+        />
+
+        <FormInput
           label="Email"
           id="email"
+          type="email"
           placeholder="example@email.com"
           value={email}
           onChange={({ target }) => setEmail(target.value)}
+          required
         />
 
         <FormInput
@@ -65,12 +105,16 @@ function SignUp() {
           placeholder="Enter your password"
           value={password}
           onChange={({ target }) => setPassword(target.value)}
+          required
         />
 
-        <FormButton type="submit">Sign Up</FormButton>
+        <FormButton type="submit" isLoading={isLoading}>
+          Sign Up
+        </FormButton>
 
         <Text mt={2}>
-          Already have an account? <Link to={ROUTES.login}>Log in</Link>
+          Already have an account?
+          <Link to={ROUTES.login}>Log in</Link>
         </Text>
       </Form>
     </Container>
