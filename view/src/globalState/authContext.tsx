04ddda1c2ea97b1e7fa231f0373734348cba7e6f
ferueda <fe.rueda@outlook.com>
2021-01-27 19:12:12 -0300

    Update to use new types and interfaces

--- view/src/globalState/authContext.tsx
+++ view/src/globalState/authContext.tsx
@@ -1,9 +1,10 @@
 import { createContext, ReactNode, useState } from 'react';
+import { IUser } from '../types/IUser';
 
 export const AuthContext = createContext<any>(null);
 
 function AuthContextProvider({ children }: { children: ReactNode }) {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<IUser | null>(null);
   return <AuthContext.Provider value={[user, setUser]}>{children}</AuthContext.Provider>;
 }
 
