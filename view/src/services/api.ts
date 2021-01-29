24e9ee35c7bb0aadba7f89b0e79ff83009300fbc
ferueda <fe.rueda@outlook.com>
2021-01-29 20:58:57 -0300

    Create getPlaces function

--- view/src/services/api.ts
+++ view/src/services/api.ts
@@ -2,7 +2,7 @@ import { ENDPOINTS } from '../utils/constants';
 import { IUser } from '../types/IUser';
 
 export async function postNewUserToDb(userData: IUser): Promise<IUser> {
-  const res = await fetch(ENDPOINTS.dev, {
+  const res = await fetch(ENDPOINTS.signUp, {
     method: 'POST',
     mode: 'cors',
     headers: {
@@ -10,5 +10,17 @@ export async function postNewUserToDb(userData: IUser): Promise<IUser> {
     },
     body: JSON.stringify(userData),
   });
+  return await res.json();
+}
+
+export async function getPlaces(token: string): Promise<any[]> {
+  const res = await fetch(ENDPOINTS.places, {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
   return await res.json();
 }
