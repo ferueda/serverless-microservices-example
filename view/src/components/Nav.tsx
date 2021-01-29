b91dfeb0f3b58f7fb2bcc3af132fd478b5f4ea02
ferueda <fe.rueda@outlook.com>
2021-01-29 20:57:41 -0300

    Improve Nav styles

--- view/src/components/Nav.tsx
+++ view/src/components/Nav.tsx
@@ -33,17 +33,28 @@ function Nav() {
       display="flex"
       justifyContent="space-between"
       shadow="md"
+      height="6vh"
+      pos="sticky"
+      top={0}
+      left={0}
+      zIndex={100}
+      bg="white"
     >
       {user ? (
         <>
           <Box>
             <NavLink to={ROUTES.home}>Home</NavLink>
           </Box>
+
           <Box d="flex" alignItems="center">
             <Box d="flex" alignItems="center" mx={2}>
-              <Icon as={BsFillPersonFill} w={5} h={5} mr={1} color="gray.600" />
-              <Text>{user?.first_name}</Text>
+              <Icon as={BsFillPersonFill} w={6} h={6} mr={1} color="green.600" />
+
+              <Text color="green.600">
+                <strong>{user?.first_name} </strong>
+              </Text>
             </Box>
+
             <Text as="button" onClick={handleLogout} _hover={{ color: 'blue.500' }} mx={2}>
               Log out
             </Text>
