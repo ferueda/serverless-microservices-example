a30c421ac7e9e9619d248fcf6bca3ee4f9608507
ferueda <fe.rueda@outlook.com>
2021-01-31 22:42:56 -0300

    Update app structure
    Changed routes name

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -9,33 +9,36 @@ import Login from './pages/Login';
 import Nav from './components/Nav';
 
 import AuthContextProvider from './globalState/AuthContext';
+import FavContextProvider from './globalState/FavContext';
 
 function App() {
   return (
     <AuthContextProvider>
-      <Box minHeight="100vh" w="100%" p={0}>
-        <Nav />
+      <FavContextProvider>
+        <Box minHeight="100vh" w="100%" p={0}>
+          <Nav />
 
-        <Container as="main" display="flex" maxW={1200} width="100%" minHeight="94vh">
-          <Switch>
-            <Route exact path={ROUTES.home}>
-              <Home />
-            </Route>
+          <Container as="main" display="flex" maxW={1200} width="100%" minHeight="94vh">
+            <Switch>
+              <Route exact path={ROUTES.home}>
+                <Home />
+              </Route>
 
-            <Route path={ROUTES.signup}>
-              <SignUp />
-            </Route>
+              <Route path={ROUTES.signup}>
+                <SignUp />
+              </Route>
 
-            <Route path={ROUTES.login}>
-              <Login />
-            </Route>
+              <Route path={ROUTES.login}>
+                <Login />
+              </Route>
 
-            <Route path="*">
-              <Redirect to={ROUTES.home} />
-            </Route>
-          </Switch>
-        </Container>
-      </Box>
+              <Route path="*">
+                <Redirect to={ROUTES.home} />
+              </Route>
+            </Switch>
+          </Container>
+        </Box>
+      </FavContextProvider>
     </AuthContextProvider>
   );
 }
