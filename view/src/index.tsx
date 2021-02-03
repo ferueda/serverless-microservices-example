b14d7ff6389f0758eab5ac9fa668d2a1d9f5bdb9
ferueda <fe.rueda@outlook.com>
2021-02-02 23:26:14 -0300

    Add Redux provider to app

--- view/src/index.tsx
+++ view/src/index.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 import { ChakraProvider, GlobalStyle } from '@chakra-ui/react';
 import theme from './styles/theme';
+import store from './store/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    <ReduxProvider store={store}>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <GlobalStyle />
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+    </ReduxProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
