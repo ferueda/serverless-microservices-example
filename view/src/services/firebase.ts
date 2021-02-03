71ac34fd7d9942025780c2d5292684c5ed209bf4
ferueda <fe.rueda@outlook.com>
2021-02-03 12:34:56 -0300

    Imports cleanup

--- view/src/services/firebase.ts
+++ view/src/services/firebase.ts
@@ -1,8 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import { Pokemon } from '../types/IPokemon';
-import { IUserData, IDbUser, AuthedUser } from '../types/IUser';
+import type { Pokemon } from '../types/IPokemon';
+import type { IUserData, IDbUser, AuthedUser } from '../types/IUser';
 import { firebaseConfig } from '../utils/config';
 import { postNewUserToDb } from './api';
 
