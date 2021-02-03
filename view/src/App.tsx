e124ddc2fda64a38099a109c1317bd1187740782
ferueda <fe.rueda@outlook.com>
2021-02-03 12:32:47 -0300

    Mayor Refactor
    Switched business logic to Redux
    Favorites and pokemons now called from App.tsx

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { ROUTES } from './utils/constants';
 import { Container, Box } from '@chakra-ui/react';
+
+import { ROUTES } from './utils/constants';
+
+import { getAllPokemons } from './store/pokemons';
+import { getFavoritePokemons } from './store/favorites';
+import type { AppState } from './store/store';
+import type { AuthState } from './store/types';
 
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
@@ -8,11 +16,22 @@ import Login from './pages/Login';
 
 import Nav from './components/Nav';
 
-import FavContextProvider from './globalState/FavContext';
-
 function App() {
+  const dispatch = useDispatch();
+  const { user } = useSelector<AppState, AuthState>((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(getAllPokemons());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (!user) return;
+
+    dispatch(getFavoritePokemons(user.uid));
+  }, [user, dispatch]);
+
   return (
-    <FavContextProvider>
+    <>
       <Box minHeight="100vh" w="100%" p={0}>
         <Nav />
 
@@ -36,7 +55,7 @@ function App() {
           </Switch>
         </Container>
       </Box>
-    </FavContextProvider>
+    </>
   );
 }
 
--- view/src/index.tsx
+++ view/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
 import { ChakraProvider, GlobalStyle } from '@chakra-ui/react';
+
 import theme from './styles/theme';
 import store from './store/store';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
