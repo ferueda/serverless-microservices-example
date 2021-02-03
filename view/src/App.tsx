b98246bf90619e5a50d0515af5b31a02b2129771
ferueda <fe.rueda@outlook.com>
2021-02-02 23:26:05 -0300

    Remove context providers
    Switched state management to redux and removed contexts providers

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -8,38 +8,35 @@ import Login from './pages/Login';
 
 import Nav from './components/Nav';
 
-import AuthContextProvider from './globalState/AuthContext';
 import FavContextProvider from './globalState/FavContext';
 
 function App() {
   return (
-    <AuthContextProvider>
-      <FavContextProvider>
-        <Box minHeight="100vh" w="100%" p={0}>
-          <Nav />
+    <FavContextProvider>
+      <Box minHeight="100vh" w="100%" p={0}>
+        <Nav />
 
-          <Container as="main" display="flex" maxW={1200} width="100%" minHeight="94vh">
-            <Switch>
-              <Route exact path={ROUTES.home}>
-                <Home />
-              </Route>
+        <Container as="main" display="flex" maxW={1200} width="100%" minHeight="94vh">
+          <Switch>
+            <Route exact path={ROUTES.home}>
+              <Home />
+            </Route>
 
-              <Route path={ROUTES.signup}>
-                <SignUp />
-              </Route>
+            <Route path={ROUTES.signup}>
+              <SignUp />
+            </Route>
 
-              <Route path={ROUTES.login}>
-                <Login />
-              </Route>
+            <Route path={ROUTES.login}>
+              <Login />
+            </Route>
 
-              <Route path="*">
-                <Redirect to={ROUTES.home} />
-              </Route>
-            </Switch>
-          </Container>
-        </Box>
-      </FavContextProvider>
-    </AuthContextProvider>
+            <Route path="*">
+              <Redirect to={ROUTES.home} />
+            </Route>
+          </Switch>
+        </Container>
+      </Box>
+    </FavContextProvider>
   );
 }
 
